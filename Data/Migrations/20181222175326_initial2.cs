a56a4006a36c6911224227dc4c40b0147e22e934
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2021-05-08 12:30:06 +0430

    Add

---
+++ Data/Migrations/20181222175326_initial2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LastLoginDate",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LastLoginDate",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+        }
+    }
+}
